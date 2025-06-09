58cc0ed4ef0275b548a7dc9ed61f4f0b919796d1
tzirkus <zalmen5779@gmail.com>
2025-06-09 18:29:12 +0300

    Person Identification Flow

--- malshinon/Program.cs
+++ malshinon/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace malshinon
+﻿using malshinon.dal;
+
+namespace malshinon
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PeopleDal P = new PeopleDal();
+            P.PersonIdentification("mus");
+
         }
     }
 }
--- malshinon/dal/PeopleDal.cs
+++ malshinon/dal/PeopleDal.cs
@@ -8,11 +8,88 @@ using System.Text;
 using System.Threading.Tasks;
 using malshinon.db;
 using malshinon.moddels;
+using System.Reflection.PortableExecutable;
 
 namespace malshinon.dal
 {
     public class PeopleDal
     {
-        
+        public MySqlData SqlData = new MySqlData();
+        public void PersonIdentification(string SecretCode)
+        {
+            string query = "SELECT * FROM people";
+            bool IsNotExists = true;
+            MySqlCommand cmd = null;
+            MySqlDataReader reader = null;
+
+            try
+            {
+                SqlData.OpenConnection();
+                cmd = new MySqlCommand(query, SqlData.connection);
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read() && IsNotExists)
+                {
+                    IsNotExists = reader.GetString("secret_code") == SecretCode ? false : true; 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                if (reader != null && !reader.IsClosed)
+                    reader.Close();
+
+                SqlData.CloseConnection();
+            }
+
+            if (IsNotExists)
+            {
+                AddPerson(CreatePerson());
+            }
+        }
+
+        public void AddPerson(Person person)
+        {
+            
+            try
+            {
+                SqlData.OpenConnection();
+                string Query = $"INSERT INTO people (first_name, last_name, secret_code, type, num_reports, num_mentions) VALUES ('{person.FirstName}', '{person.LastName}' , '{person.SecretCode}', '{person.Type}', '{person.NumReports}', '{person.NumMention}');";
+                MySqlCommand cmd = new MySqlCommand(Query, SqlData.connection);
+                cmd.ExecuteNonQuery();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error : {ex.Message}");
+            }
+            finally
+            {
+                SqlData.CloseConnection();
+            }
+        }
+
+        public Person CreatePerson()
+        {
+            Console.WriteLine("enter first name");
+            string FirstName = Console.ReadLine()!;
+            Console.WriteLine("enter last name");
+            string LastName = Console.ReadLine()!;
+            Console.WriteLine("enter secret code");
+            string SecretCode = Console.ReadLine()!;
+            Console.WriteLine("enter type, (reporter, target, both, potential_agent)");
+            string Type = Console.ReadLine()!;
+            Console.WriteLine("enter num report");
+            string NumReports = Console.ReadLine()!;
+            Console.WriteLine("enter num mention");
+            string NumMention = Console.ReadLine()!;
+            Console.WriteLine("enter first status (dangerous)");
+            string Status = Console.ReadLine()!;
+            Person person = new Person(FirstName,LastName,SecretCode,Type,NumReports ,NumMention); 
+            return person;
+        }
     }
 }
